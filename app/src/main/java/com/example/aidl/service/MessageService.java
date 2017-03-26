269dd398edfdaeff97f64f536d42511cdfe4e884
chenjianwei <938548157@qq.com>
2017-03-26 23:50:32 +0800

    Step.1

---
+++ app/src/main/java/com/example/aidl/service/MessageService.java
@@ -0,0 +1,16 @@
+package com.example.aidl.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MessageService extends Service {
+    public MessageService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
