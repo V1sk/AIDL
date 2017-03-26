96e000b249e05da4fe1a27710a67e669ff7b4d2f
chenjianwei <938548157@qq.com>
2017-03-27 01:09:45 +0800

    Step.2

--- app/src/main/java/com/example/aidl/ui/MainActivity.java
+++ app/src/main/java/com/example/aidl/ui/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.aidl.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.aidl.R;
+import com.example.aidl.service.MessageService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupService();
     }
+
+    /**
+     * unbindService
+     */
+    @Override
+    protected void onDestroy() {
+        unbindService(serviceConnection);
+        super.onDestroy();
+    }
+
+    /**
+     * bindService & startService：
+     * 使用bindService方式，多个Client可以同时bind一个Service，但是当所有Client unbind后，Service会退出
+     * 通常情况下，如果希望和Service交互，一般使用bindService方法，获取到onServiceConnected中的IBinder对象，和Service进行交互，
+     * 不需要和Service交互的情况下，使用startService方法即可，Service主线程执行完成后会自动关闭；
+     * unbind后Service仍保持运行，可以同时调用bindService和startService（比如像聊天软件，退出UI进程，Service仍能接收消息）
+     */
+    private void setupService() {
+        Intent intent = new Intent(this, MessageService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        startService(intent);
+    }
+
+    ServiceConnection serviceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "onServiceConnected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "onServiceDisconnected");
+        }
+    };
+
 }
